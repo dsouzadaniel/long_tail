4f5704f554b38e92d3a437115e283b7d791c8d5b
ddsouzapq1 <Daniel.Dsouza@proquest.com>
2022-01-09 01:57:37 -0500

    # 10000 examples

--- imagenet_on_gpu/classes.py
+++ imagenet_on_gpu/classes.py
@@ -109,6 +109,7 @@ class IMAGENET(DatasetFolder):
             raise FileNotFoundError(msg)
 
         shuffle(instances)
+        print("###"*10, set([i[1] for i in instances[:10000]]))
         return instances[:10000]
 
         # return instances
@@ -233,7 +234,6 @@ class IMAGENET_DYNAMIC(DatasetFolder):
             raise FileNotFoundError(msg)
 
         shuffle(instances)
-        print("***"*10, set([i[1] for i in instances[:10000]]))
         return instances[:10000]
 
         # return instances
