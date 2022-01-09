80b2c014228eed575bf416a76132bf0f0f6d8079
ddsouzapq1 <Daniel.Dsouza@proquest.com>
2022-01-09 03:01:51 -0500

    # 10000 examples

--- imagenet_on_gpu/classes.py
+++ imagenet_on_gpu/classes.py
@@ -109,8 +109,8 @@ class IMAGENET(DatasetFolder):
             raise FileNotFoundError(msg)
 
         shuffle(instances)
-        # print("###"*10, len(list(set([i[1] for i in instances[:10000]]))))
-        return instances[:10000]
+        # print("###"*10, len(list(set([i[1] for i in instances[:50000]]))))
+        return instances[:50000]
 
         # return instances
 
@@ -234,7 +234,7 @@ class IMAGENET_DYNAMIC(DatasetFolder):
             raise FileNotFoundError(msg)
 
         shuffle(instances)
-        return instances[:10000]
+        return instances[:50000]
 
         # return instances
 
@@ -285,9 +285,9 @@ class LONGTAIL_IMAGENET(DatasetFolder):
         self.selected_ixs_for_atypical = np.where(_indicator == -2)[0]
         self.selected_ixs_for_typical = np.where(_indicator == -1)[0]
 
-        self.selected_ixs_for_noisy = self.selected_ixs_for_noisy[self.selected_ixs_for_noisy<10000]
-        self.selected_ixs_for_atypical = self.selected_ixs_for_atypical[self.selected_ixs_for_atypical<10000]
-        self.selected_ixs_for_typical = self.selected_ixs_for_typical[self.selected_ixs_for_typical<10000]
+        self.selected_ixs_for_noisy = self.selected_ixs_for_noisy[self.selected_ixs_for_noisy<50000]
+        self.selected_ixs_for_atypical = self.selected_ixs_for_atypical[self.selected_ixs_for_atypical<50000]
+        self.selected_ixs_for_typical = self.selected_ixs_for_typical[self.selected_ixs_for_typical<50000]
 
         self.class_ixs = [np.where(self.labels == c)[0] for c in range(len(classes))]
         self.num_of_dupes = self._dataset_npz['num_dupes_data'].item()
@@ -320,7 +320,7 @@ class LONGTAIL_IMAGENET(DatasetFolder):
 
     def make_dataset(self, directory: str, class_2_ix: Dict[str, int]):
         dataset = [(os.path.join(directory, str(d[0]).split('_')[0], str(d[0])), str(d[1])) for d in
-                   self._dataset_npz['filenames']][:10000]
+                   self._dataset_npz['filenames']][:50000]
         return dataset
 
     def __repr__(self):
@@ -423,5 +423,5 @@ class LONGTAIL_IMAGENET_DYNAMIC(DatasetFolder):
 
     def make_dataset(self, directory: str, class_2_ix: Dict[str, int]):
         dataset = [(os.path.join(directory, str(d[0]).split('_')[0], str(d[0])), str(d[1])) for d in
-                   self._dataset_npz['filenames']][:10000]
+                   self._dataset_npz['filenames']][:50000]
         return dataset
